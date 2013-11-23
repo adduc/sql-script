a4b07b20dcb42dfaa7673d96183925d133ebb570
John Long <a@88k.us>
2013-11-23 02:03:46 -0600

    Adding documentation.

--- src/Configuration.php
+++ src/Configuration.php
@@ -8,31 +8,29 @@ namespace Adduc\SqlScript;
 class Configuration
 {
 
+    /** @var string File containing database configuration. */
     public $database_file = "config/database.json";
 
-    /**
-     *
-     */
+    /** @var string Directory containing SQL schema files to run. */
     public $sql_schema = "sql/schema";
 
-    /**
-     *
-     */
+    /** @var string Directory containing SQL data files to run. */
     public $sql_data = "sql/data";
 
-    /**
-     *
-     */
+    /** @var array Database configuration (loaded from database file.) */
     public $database = array(
         "dsn" => "mysql:dbname=database;host=127.0.0.1",
         "username" => "username",
         "password" => "password"
     );
 
-    public function __construct(array $data = array()) {
+    /**
+     * @param array $data
+     */
+    public function __construct(array $data = array())
+    {
         foreach ($data as $key => $value) {
             $this->$key = $value;
         }
     }
-
 }
