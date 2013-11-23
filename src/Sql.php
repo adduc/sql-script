99b11a4eba415e82e092bf43e8aacc3e4fa3905d
John Long <a@88k.us>
2013-11-23 02:21:41 -0600

    Working?

--- src/Sql.php
+++ src/Sql.php
@@ -25,6 +25,19 @@ class Sql
             $config->database['username'],
             $config->database['password']
         );
+        $output && fwrite($output, "Connected to database.\n");
+
+        foreach (array('sql_schema', 'sql_data') as $key) {
+
+            $dir = dirname($file) . "/{$config->$key}";
+            file_exists($dir) || $dir = $config->$key;
+            $files = array_diff(scandir($dir), array('.', '..'));
+
+            foreach ($files as $sql) {
+                $output && fwrite($output, sprintf("Processing %s.\n", $sql));
+                $db->exec("SOURCE {$sql};");
+            }
+        }
     }
 
 }
--- tests/SqlTest.php
+++ tests/SqlTest.php
@@ -18,13 +18,42 @@ class SqlTest extends \PHPUnit_Framework_TestCase
     public function testRun()
     {
         $root = vfsStream::setup('root', null, array(
-            'composer.json' => '{"extra": {"adduc-sql": {"database_file": "database.json"}}}',
-            'database.json' => '{"dsn": "sqlite::memory:"}'
+            'composer.json' => '{"extra": { "adduc-sql": {
+                "database_file": "database.json",
+                "sql_schema": "sql_schema",
+                "sql_data": "sql_data"
+            }}}',
+            'database.json' => '{"dsn": "sqlite::memory:"}',
+            "sql_schema" => array(
+                "schema.test.sql" => "SQL ERROR"
+            ),
+            "sql_data" => array(
+                "data.test.sql" => ""
+            )
         ));
 
         $dir = vfsStream::url('root');
-        $result = $this->sql->run($dir);
-        $this->assertEmpty($result);
+        $stream = fopen("php://memory", 'r+');
+        $result = $this->sql->run($dir, $stream);
+
+        $output = $this->readStream($stream);
+        $this->assertContains('schema.test.sql', $output);
+        $this->assertContains('data.test.sql', $output);
     }
 
+    /**
+     * Given a stream, rewind and read all its contents.
+     *
+     * @param resource $stream
+     * @return string
+     */
+    protected function readStream($stream)
+    {
+        rewind($stream);
+        $buffer = "";
+        while (!feof($stream)) {
+            $buffer .= fgets($stream);
+        }
+        return $buffer;
+    }
 }
