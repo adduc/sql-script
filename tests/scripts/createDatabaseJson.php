6b8dea145cec54f950bff0f45f592f927b367af1
John Long <a@88k.us>
2013-11-25 02:44:59 -0600

    Adding database create script for use by build server.

--- tests/scripts/createDatabaseJson.php
+++ tests/scripts/createDatabaseJson.php
@@ -1,3 +1,4 @@
+#!/usr/bin/env php
 <?php
 
 /**
@@ -6,4 +7,37 @@
  *
  * It is provided for use by the build server for this repository, and may
  * change at any time.
+ *
+ * Example Usage: php createDatabaseJson.php hostname=h\&username=u\&password=p\&database=d
  */
+
+namespace Adduc\SqlScript;
+
+if (php_sapi_name() != 'cli') {
+    die("This script can only be run via command line.");
+}
+
+require(__DIR__ . '/../../vendor/autoload.php');
+
+
+try {
+    // Read in arguments.
+    parse_str(end($argv), $data);
+
+    // Validate Data.
+    $db_config = new DatabaseConfig();
+    $db_config->validateDatabaseConfig($data);
+
+    $file = __DIR__ . '/../../database.json';
+    if(file_exists($file)) {
+        throw new \Exception("File already exists. Will not overwrite.\n");
+    }
+
+    fwrite(STDOUT, "Writing to {$file}");
+    file_put_contents($file, json_encode($data));
+
+} catch (\Exception $e) {
+    fwrite(STDERR, "{$e->getMessage()}\n");
+    exit(1);
+}
+
